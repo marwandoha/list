adb37f7a60ad52442bae9971867d24259273ccb0
Marwan <marwan.doha@live.com>
2014-03-29 00:53:55 +0200

    modified app

---
+++ www/views/home.js
@@ -0,0 +1,16 @@
+﻿MyApp.home = function (params) {
+    var viewModel = {
+        dataSource: new DevExpress.data.DataSource({
+            load: function(loadOptions) {
+                return $.getJSON('http://sampleservices.devexpress.com/api/Categories');
+            },
+            map: function(item) {
+                return {
+                    id: item.CategoryID,
+                    name: item.CategoryName
+                };
+            }                
+        })
+    };
+    return viewModel;
+};
