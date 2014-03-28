8d7cfdf248cf9d91a04f7a6f0854269f8bea84c2
Marwan <marwan.doha@live.com>
2014-03-29 01:32:07 +0200

    modified app

---
+++ www/index.js
@@ -0,0 +1,51 @@
+﻿window.MyApp = {};
+
+$(function() {
+    MyApp.app = new DevExpress.framework.html.HtmlApplication({
+        namespace: MyApp,
+        
+        navigationType: "navbar",
+        navigation: [
+          {
+            title: "Categories",
+            action: "#home",
+            icon: "home"
+          },
+          {
+              title: "About",
+              action: "#about",
+              icon: "info"
+          }
+        ],
+        commandMapping: {
+            "ios-header-toolbar": {
+
+                commands: [
+                    { id: "search", location: 'right', showText: false }
+                ]
+            },
+            "android-simple-toolbar": {
+                commands: [
+                    { id: "search", location: 'right', showText: false }
+                ]
+            },
+            "tizen-footer-toolbar": {
+                commands: [
+                     { id: "search", location: 'center', showText: false }
+                ]
+            },
+            "generic-header-toolbar": {
+                commands: [
+                    { id: "search", location: 'right', showText: false }
+                ]
+            },
+            "win8-phone-appbar": {
+                commands: [
+                    { id: "search", location: 'center', showText: true }
+                ]
+            },
+        }
+    });
+    MyApp.app.router.register(":view/:id", { view: "home", id: undefined });
+    MyApp.app.navigate();   
+});
